7afa57c440235e15f741ccb61f6ec8cf5f4dc090
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-02-08 23:58:38 -0300

    slight alterations to icons

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/favicon.ico',
+  manifestIcon: 'src/assets/img/favicon-3.png',
   pathPrefix: `/gatsby-starter-grayscale/`, // This path is subpath of your hosting https://domain/portfolio
   subHeading: 'rapper, singer, songwriter',
 
