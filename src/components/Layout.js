818bf4b00dd450795a733bd820752c70c42ecf7b
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-09 15:18:27 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+{/*PropTypes exports a range of validators that can be used to make sure the data you receive is valid.*/}
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 {/*when react helmet is used with server-side rendering, it allows you to set meta tags that will be read by search engines and social media crawlers.*/}
